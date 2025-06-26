fa8d5ddf6d97d4f81479c2ecd7b1369fb5ac8683
Lars Kellogg-Stedman <lars@oddbit.com>
2025-06-26 19:59:57 -0400

    Initial import

---
+++ main.go
@@ -0,0 +1,347 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	"github.com/emersion/go-message/mail"
+)
+
+type Config struct {
+	IMAP struct {
+		Server   string `json:"server"`
+		Port     int    `json:"port"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+		UseSSL   bool   `json:"use_ssl"`
+		Mailbox  string `json:"mailbox"`
+	} `json:"imap"`
+	Monitor struct {
+		RegexPattern  string `json:"regex_pattern"`
+		Command       string `json:"command"`
+		CheckInterval int    `json:"check_interval_seconds"`
+	} `json:"monitor"`
+}
+
+type EmailMonitor struct {
+	config      Config
+	client      *client.Client
+	regex       *regexp.Regexp
+	lastUID     uint32
+	reconnectCh chan bool
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <config.json>")
+		os.Exit(1)
+	}
+
+	configFile := os.Args[1]
+	config, err := loadConfig(configFile)
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	monitor, err := NewEmailMonitor(config)
+	if err != nil {
+		log.Fatalf("Failed to create monitor: %v", err)
+	}
+
+	log.Println("Starting email monitor...")
+	monitor.Start()
+}
+
+func loadConfig(filename string) (Config, error) {
+	var config Config
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	return config, err
+}
+
+func NewEmailMonitor(config Config) (*EmailMonitor, error) {
+	regex, err := regexp.Compile(config.Monitor.RegexPattern)
+	if err != nil {
+		return nil, fmt.Errorf("invalid regex pattern: %v", err)
+	}
+
+	monitor := &EmailMonitor{
+		config:      config,
+		regex:       regex,
+		reconnectCh: make(chan bool, 1),
+	}
+
+	return monitor, nil
+}
+
+func (em *EmailMonitor) Start() {
+	for {
+		err := em.connect()
+		if err != nil {
+			log.Printf("Connection failed: %v. Retrying in 30 seconds...", err)
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		log.Println("Connected to IMAP server")
+
+		// Get initial state
+		err = em.initializeLastUID()
+		if err != nil {
+			log.Printf("Failed to initialize: %v", err)
+			em.disconnect()
+			continue
+		}
+
+		// Start monitoring
+		err = em.monitor()
+		if err != nil {
+			log.Printf("Monitor error: %v. Reconnecting...", err)
+		}
+
+		em.disconnect()
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func (em *EmailMonitor) connect() error {
+	var c *client.Client
+	var err error
+
+	address := fmt.Sprintf("%s:%d", em.config.IMAP.Server, em.config.IMAP.Port)
+
+	if em.config.IMAP.UseSSL {
+		c, err = client.DialTLS(address, &tls.Config{})
+	} else {
+		c, err = client.Dial(address)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if err := c.Login(em.config.IMAP.Username, em.config.IMAP.Password); err != nil {
+		c.Close()
+		return err
+	}
+
+	em.client = c
+	return nil
+}
+
+func (em *EmailMonitor) disconnect() {
+	if em.client != nil {
+		em.client.Close()
+		em.client = nil
+	}
+}
+
+func (em *EmailMonitor) initializeLastUID() error {
+	mailbox := em.config.IMAP.Mailbox
+	if mailbox == "" {
+		mailbox = "INBOX"
+	}
+
+	mbox, err := em.client.Select(mailbox, false)
+	if err != nil {
+		return err
+	}
+
+	em.lastUID = mbox.UidNext - 1
+	log.Printf("Initialized with last UID: %d", em.lastUID)
+	return nil
+}
+
+func (em *EmailMonitor) monitor() error {
+	mailbox := em.config.IMAP.Mailbox
+	if mailbox == "" {
+		mailbox = "INBOX"
+	}
+
+	checkInterval := time.Duration(em.config.Monitor.CheckInterval) * time.Second
+	if checkInterval == 0 {
+		checkInterval = 30 * time.Second
+	}
+
+	ticker := time.NewTicker(checkInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := em.checkForNewMessages()
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (em *EmailMonitor) checkForNewMessages() error {
+	mailbox := em.config.IMAP.Mailbox
+	if mailbox == "" {
+		mailbox = "INBOX"
+	}
+
+	_, err := em.client.Select(mailbox, false)
+	if err != nil {
+		return err
+	}
+
+	// Search for messages with UID greater than lastUID
+	criteria := imap.NewSearchCriteria()
+	criteria.Uid = new(imap.SeqSet)
+	criteria.Uid.AddRange(em.lastUID+1, 0)
+
+	uids, err := em.client.UidSearch(criteria)
+	if err != nil {
+		return err
+	}
+
+	if len(uids) == 0 {
+		return nil
+	}
+
+	log.Printf("Found %d new messages", len(uids))
+
+	// Fetch new messages
+	seqset := new(imap.SeqSet)
+	for _, uid := range uids {
+		seqset.AddNum(uid)
+	}
+
+	messages := make(chan *imap.Message, 10)
+	done := make(chan error, 1)
+
+	go func() {
+		done <- em.client.UidFetch(seqset, []imap.FetchItem{imap.FetchEnvelope, imap.FetchBodyStructure, "BODY[]"}, messages)
+	}()
+
+	for msg := range messages {
+		err := em.processMessage(msg)
+		if err != nil {
+			log.Printf("Error processing message: %v", err)
+		}
+
+		if msg.Uid > em.lastUID {
+			em.lastUID = msg.Uid
+		}
+	}
+
+	if err := <-done; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (em *EmailMonitor) processMessage(msg *imap.Message) error {
+	log.Printf("Processing message from: %s, Subject: %s",
+		msg.Envelope.From[0].Address(), msg.Envelope.Subject)
+
+	// Get message body
+	for _, part := range msg.Body {
+		body, err := em.extractTextFromPart(part)
+		if err != nil {
+			log.Printf("Error extracting text: %v", err)
+			continue
+		}
+
+		if body == "" {
+			continue
+		}
+
+		// Check if body matches regex
+		if em.regex.MatchString(body) {
+			log.Printf("Regex match found in message from: %s", msg.Envelope.From[0].Address())
+
+			err := em.executeCommand(msg, body)
+			if err != nil {
+				log.Printf("Error executing command: %v", err)
+			}
+
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func (em *EmailMonitor) extractTextFromPart(part io.Reader) (string, error) {
+	mr, err := mail.CreateReader(part)
+	if err != nil {
+		return "", err
+	}
+
+	var body strings.Builder
+
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return "", err
+		}
+
+		switch h := p.Header.(type) {
+		case *mail.InlineHeader:
+			mediaType, _, _ := h.ContentType()
+			if strings.HasPrefix(mediaType, "text/") {
+				content, err := io.ReadAll(p.Body)
+				if err != nil {
+					continue
+				}
+				body.WriteString(string(content))
+			}
+		}
+	}
+
+	return body.String(), nil
+}
+
+func (em *EmailMonitor) executeCommand(msg *imap.Message, body string) error {
+	if em.config.Monitor.Command == "" {
+		log.Println("No command configured")
+		return nil
+	}
+
+	// Set environment variables with message info
+	env := os.Environ()
+	env = append(env, fmt.Sprintf("EMAIL_FROM=%s", msg.Envelope.From[0].Address()))
+	env = append(env, fmt.Sprintf("EMAIL_SUBJECT=%s", msg.Envelope.Subject))
+	env = append(env, fmt.Sprintf("EMAIL_DATE=%s", msg.Envelope.Date.Format(time.RFC3339)))
+	env = append(env, fmt.Sprintf("EMAIL_UID=%d", msg.Uid))
+
+	// Parse command (simple shell command parsing)
+	parts := strings.Fields(em.config.Monitor.Command)
+	if len(parts) == 0 {
+		return fmt.Errorf("empty command")
+	}
+
+	cmd := exec.Command(parts[0], parts[1:]...)
+	cmd.Env = env
+	cmd.Stdin = strings.NewReader(body)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("Command execution failed: %v, Output: %s", err, string(output))
+		return err
+	}
+
+	log.Printf("Command executed successfully. Output: %s", string(output))
+	return nil
+}
