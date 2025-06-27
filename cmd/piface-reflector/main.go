9b730b4a73ab99618036b1d5cd7044a876f864b5
Lars Kellogg-Stedman <lars@oddbit.com>
2025-06-27 14:02:46 -0400

    feat: We have liftoff!
    The example input->output reflector in cmd/piface-reflector now works successfully
    using the internal/piface package.

---
+++ cmd/piface-reflector/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/larsks/airdancer/internal/piface" // Replace with actual module path
+)
+
+func main() {
+	spiPortName := "/dev/spidev0.0"
+	pf, err := piface.NewPiFace(spiPortName)
+	if err != nil {
+		log.Fatal("Failed to initialize piface:", err)
+	}
+	defer pf.Close()
+
+	// Reflect inputs to outputs
+	var oldval uint8
+	for {
+		val, err := pf.ReadInputs()
+		if err != nil {
+			log.Fatal("failed to read inputs:", err)
+		}
+
+		if val != oldval {
+			fmt.Printf("INPUTS: ")
+			for i := range 8 {
+				fmt.Printf("%d ", (val>>(7-i))&0x1)
+			}
+			fmt.Println("")
+			oldval = val
+		}
+
+		if err := pf.WriteOutputs(val); err != nil {
+			log.Fatal("failed to write outputs:", err)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+}
