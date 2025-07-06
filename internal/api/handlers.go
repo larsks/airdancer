fada982dc78cd9de67222845d8cc774f856b83d2
Lars Kellogg-Stedman <lars@oddbit.com>
2025-07-06 07:59:52 -0400

    Remove unused blinkStatusHandler

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -330,28 +330,3 @@ func (s *Server) listRoutesHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]any{"routes": s.ListRoutes()}
 	s.sendSuccess(w, data)
 }
-
-func (s *Server) blinkStatusHandler(w http.ResponseWriter, r *http.Request) {
-	switchIDStr := chi.URLParam(r, "id")
-	id, _ := strconv.Atoi(switchIDStr) // No error check needed, already validated
-
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	// Get switch - no error check needed, already validated by middleware
-	sw, _ := s.switches.GetSwitch(uint(id))
-	swid := sw.String()
-	blinker, exists := s.blinkers[swid]
-
-	data := map[string]any{
-		"blinking": false,
-	}
-
-	if exists && blinker.IsRunning() {
-		data["blinking"] = true
-		data["period"] = blinker.GetPeriod()
-		data["dutyCycle"] = blinker.GetDutyCycle()
-	}
-
-	s.sendSuccess(w, data)
-}
