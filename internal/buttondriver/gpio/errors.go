ee8654739ca8113500785b8f43dad91a314e3474
Lars Kellogg-Stedman <lars@oddbit.com>
2025-07-10 23:33:47 -0400

    First gpio button driver

---
+++ internal/buttondriver/gpio/errors.go
@@ -0,0 +1,10 @@
+package gpio
+
+import "errors"
+
+var (
+	ErrPinNotFound     = errors.New("GPIO pin not found")
+	ErrPinConfig       = errors.New("failed to configure GPIO pin")
+	ErrPeriphInit      = errors.New("failed to initialize periph.io")
+	ErrNoPinsConfigured = errors.New("no GPIO pins configured")
+)
